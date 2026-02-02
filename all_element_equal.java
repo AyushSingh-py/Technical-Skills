fb680c893ab65712af7ff391bab3c97aa2f21ddc
AYUSH SINGH <ayushsingh.official.1310@gmail.com>
2026-02-02 10:48:56 +0530

    changes and added comments

--- all_element_equal.java
+++ all_element_equal.java
@@ -1,5 +1,12 @@
 //given an integer array of size n in one second u can increase the value of one element by 1. 
 // find the minimum time in seconds to make all the element of the array equal.
+
+// Observation - to minimize the time,make all element equal to the maximum element in the array.
+
+// Pusedo Code - find the mx element in array.
+//               for every element calculate how much it needs to be increased to reach the maximum 
+//               sum of all the differences.that sum equals to your time.
+
 public class all_element_equal {
     public static void main(String[] args) {
         int[] arr={1,4,5,2,6,7};
@@ -16,3 +23,4 @@ public class all_element_equal {
         System.out.println(sum);
     }
 }
+
